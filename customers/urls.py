4aae2c6a51c0d1366542198b59902facd3e2f6fb
twonumberfortyfives <sma4no.7@gmail.com>
2024-06-10 19:30:37 +0300

    added urls for views

--- customers/urls.py
+++ customers/urls.py
@@ -5,10 +5,14 @@ from rest_framework_simplejwt.views import (
     TokenVerifyView,
 )
 
+from .views import CreateUserView, LoginUserView
+
 app_name = 'customers'
 
 urlpatterns = [
+    path('register/', CreateUserView.as_view(), name='register'),
+    path('me/', LoginUserView.as_view(), name='manage-user'),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
