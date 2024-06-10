52942483648e55b23ae52af5668952c741c268c4
twonumberfortyfives <sma4no.7@gmail.com>
2024-06-10 18:26:41 +0300

    created base code for customer(user) model

--- customers/models.py
+++ customers/models.py
@@ -1,6 +1,11 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import gettext as _
 
 
 class Customer(AbstractUser):
-    pass
+    username = None
+    email = models.EmailField(_('email address'), unique=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
