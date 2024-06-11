a67924b28da5f88448bec110d0c7f1543f8b877a
twonumberfortyfives <sma4no.7@gmail.com>
2024-06-11 22:36:13 +0300

    permissions code refactoring

--- borrowing/permissions.py
+++ borrowing/permissions.py
@@ -3,10 +3,9 @@ from rest_framework.permissions import BasePermission
 
 class IsAuthenticatedEmailVerifiedReadOnlyAdminAll(BasePermission):
     def has_permission(self, request, view):
-        if request.user and request.user.is_authenticated and request.user.is_verified == 1 and view.method in (
-                "create",
-                "list",
-                "retrieve"
+        if request.user and request.user.is_authenticated and request.user.is_verified == 1 and request.method in (
+                "GET",
+                "POST",
         ):
             return True
         elif request.user.is_authenticated and request.user.is_staff:
