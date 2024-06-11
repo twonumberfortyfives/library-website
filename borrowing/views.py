e391e4c64c4438b7c0eba47e82cd7e5c4b85e5c5
twonumberfortyfives <sma4no.7@gmail.com>
2024-06-11 22:18:02 +0300

    added permission

--- borrowing/views.py
+++ borrowing/views.py
@@ -1,13 +1,14 @@
-from django.shortcuts import render
 from rest_framework import viewsets
 
 from borrowing.models import Borrowing
 from borrowing.serializers import BorrowingSerializer, BorrowingListSerializer
+from books.permissions import IsAuthenticatedAndEmailVerifiedReadOnly
 
 
 class BorrowViewSet(viewsets.ModelViewSet):
     serializer_class = BorrowingSerializer
     queryset = Borrowing.objects.all()
+    permission_classes = (IsAuthenticatedAndEmailVerifiedReadOnly,)
 
     def get_serializer_class(self):
         if self.action in ('list', 'retrieve'):
