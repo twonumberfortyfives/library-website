485ff903e2181ce17a0c9d092e1ad6544ddb7002
twonumberfortyfives <sma4no.7@gmail.com>
2024-06-11 21:29:09 +0300

    added basic serializer class for borrowing and viewset

---
+++ borrowing/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+
+from borrowing.models import Borrowing
+
+
+class BorrowingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Borrowing
+        fields = '__all__'
--- borrowing/views.py
+++ borrowing/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from borrowing.models import Borrowing
+from borrowing.serializers import BorrowingSerializer
+
+
+class BorrowViewSet(viewsets.ModelViewSet):
+    serializer_class = BorrowingSerializer
+    queryset = Borrowing.objects.all()
