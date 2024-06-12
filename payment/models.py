bb8bb73e7ec5541ab8fdc47dabdd96818137ab34
twonumberfortyfives <sma4no.7@gmail.com>
2024-06-12 19:23:11 +0300

    implemented model for payment

--- payment/models.py
+++ payment/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+from borrowing.models import Borrowing
+
+
+class Payment(models.Model):
+    CHOICES_STATUS = (
+        ("pending", "PENDING"),
+        ("paid", "PAID"),
+    )
+    CHOICES_TYPE = (
+        ("payment", "PAYMENT"),
+        ("fine", "FINE")
+    )
+    status = models.CharField(choices=CHOICES_STATUS, max_length=10, default="pending")
+    type = models.CharField(choices=CHOICES_TYPE, max_length=10, default="payment")
+    borrowing = models.ForeignKey(Borrowing, on_delete=models.CASCADE, related_name="payments")
+    session_url = models.URLField(max_length=10000)
+    session_id = models.CharField(max_length=10000)
+    money_to_pay = models.DecimalField(max_digits=10, decimal_places=2, default=0)
