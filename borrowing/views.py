a38377f3ad2f5c2f3b17eec838cb89a212295fa6
twonumberfortyfives <sma4no.7@gmail.com>
2024-06-12 18:29:34 +0300

    made response page look default drf page

--- borrowing/views.py
+++ borrowing/views.py
@@ -1,6 +1,6 @@
 import datetime
 
-from django.http import JsonResponse
+from rest_framework.response import Response
 from rest_framework import viewsets, status
 from rest_framework.decorators import api_view, permission_classes
 
@@ -33,10 +33,12 @@ class BorrowViewSet(viewsets.ModelViewSet):
 @permission_classes((IsAuthenticatedEmailVerifiedReadOnlyAdminAll,))
 def return_book(request, pk):
     borrowing_instance = Borrowing.objects.get(pk=pk)
-    if borrowing_instance.actual_return_date:
-        return JsonResponse({'message': 'The book has been returned'}, status=status.HTTP_404_NOT_FOUND)
-    borrowing_instance.actual_return_date = datetime.date.today()
-    borrowing_instance.book.inventory += 1
-    borrowing_instance.book.save()
-    borrowing_instance.save()
-    return JsonResponse({'message': 'You returned the book. Thank you!'}, status=status.HTTP_200_OK)
+    if request.method == 'GET' and borrowing_instance.user.id == request.user.id:
+        if borrowing_instance.actual_return_date:
+            return Response({'message': 'The book has been returned'}, status=status.HTTP_404_NOT_FOUND)
+        borrowing_instance.actual_return_date = datetime.date.today()
+        borrowing_instance.book.inventory += 1
+        borrowing_instance.book.save()
+        borrowing_instance.save()
+        return Response({'message': 'You returned the book. Thank you!'}, status=status.HTTP_200_OK)
+    return Response({'message': 'Not Found'}, status=status.HTTP_404_NOT_FOUND)
