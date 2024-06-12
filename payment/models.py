ae7859d2e073c2abd537ddef326ec4d87eadf8da
twonumberfortyfives <sma4no.7@gmail.com>
2024-06-12 19:31:35 +0300

    payment model updated money_to_pay field

--- payment/models.py
+++ payment/models.py
@@ -1,3 +1,5 @@
+from decimal import Decimal
+
 from django.db import models
 
 from borrowing.models import Borrowing
@@ -17,4 +19,12 @@ class Payment(models.Model):
     borrowing = models.ForeignKey(Borrowing, on_delete=models.CASCADE, related_name="payments")
     session_url = models.URLField(max_length=10000)
     session_id = models.CharField(max_length=10000)
-    money_to_pay = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+
+    @property
+    def money_to_pay(self):
+        if self.borrowing.actual_return_date:
+            period_of_borrowing = (self.borrowing.actual_return_date - self.borrowing.borrow_date).days
+            borrowing_money = Decimal(period_of_borrowing) * Decimal(self.borrowing.book.daily_fee)
+            return borrowing_money
+        else:
+            return None
