a2fa8c201c90f6d1378e4edbd004783257f66390
twonumberfortyfives <sma4no.7@gmail.com>
2024-06-13 23:21:02 +0300

    added filters for books

--- books/views.py
+++ books/views.py
@@ -12,6 +12,16 @@ class BookViewSet(viewsets.ModelViewSet):
         IsAuthenticatedReadOnlyAdminAll,
     ]
 
+    def get_queryset(self):
+        queryset = self.queryset
+        title = self.request.query_params.get('title', None)
+        author = self.request.query_params.get('author', None)
+        if title:
+            queryset = queryset.filter(title__icontains=title)
+        if author:
+            queryset = queryset.filter(author__icontains=author)
+        return queryset
+
     def get_serializer_class(self):
         if self.action in ("list", "retrieve"):
             return BookListSerializer
