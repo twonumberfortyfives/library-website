239bafcb797b4ef9a8535ee18398ad5d835d5d18
twonumberfortyfives <sma4no.7@gmail.com>
2024-06-13 21:50:19 +0300

    added admin panel for payments

--- payment/admin.py
+++ payment/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from payment.models import Payment
 
-# Register your models here.
+
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ("status", "type", "borrowing", "session_url", "session_id", "user")
+    list_filter = ("status", "type", "borrowing", "session_url", "session_id", "user")
+    search_fields = ("status", "type", "borrowing", "session_url", "session_id", "user")
+
+
+admin.site.register(Payment, PaymentAdmin)
