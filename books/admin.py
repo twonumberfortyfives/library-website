97eba27b7f21e8d3767d0ce1e42d950dbf942bbb
twonumberfortyfives <sma4no.7@gmail.com>
2024-06-13 21:43:11 +0300

    added admin panel for books

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from books.models import Book
+
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ("title", "author", "cover", "daily_fee", "inventory")
+    search_fields = ("title", "author", "cover", "daily_fee", "inventory")
+    list_filter = ("title", "author", "cover", "daily_fee", "inventory")
+
+
+admin.site.register(Book, BookAdmin)
